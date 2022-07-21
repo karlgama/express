656120617bee4cd1f6a7f2432cf5121d19d90357
kaique <kaiquesilva49@hotmail.com>
2022-07-21 19:16:42 -0300

    criando controller

--- src/app.js
+++ src/app.js
@@ -1,46 +1,34 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js"
+import livros from "./models/Livro.js";
+import routes from "./Routes/index.js";
 
-db.on("error",console.log.bind(console, "Erro de conexao"))
-db.once("open",()=>{
-    console.log("conexão realizada com sucesso")
-})
+db.on("error", console.log.bind(console, "Erro de conexao"));
+db.once("open", () => {
+  console.log("conexão realizada com sucesso");
+});
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("curso node");
+app.put("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status("200").json(livros);
 });
 
-app.get("/livros",(req,res)=>{
-    livros.find((err,livros)=>{
-        res.status(200).json(livros)
-    })
-})
-
-app.post("/livros",(req,res)=>{
-    livros.push(req.body)
-    res.status("201").json(livros)
-})
-
-app.put("/livros/:id",(req,res)=>{
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo=req.body.titulo;
-    res.status("200").json(livros);
-})
-
-app.delete("/livros/:id",(req,res)=>{
-    let {id} = req.params
-    let index = buscaLivro(id)
-    livros.splice(index,1)
-    res.send(`livro ${id} removido`)
-})
+app.delete("/livros/:id", (req, res) => {
+  let { id } = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`livro ${id} removido`);
+});
 
 function buscaLivro(id) {
-    return livros.findIndex((livro)=>livro.id==id)
+  return livros.findIndex((livro) => livro.id == id);
 }
 
-export default app
+routes(app);
+
+export default app;
